4c3a1dc651cee0c17d1ea439cea77a25406643de
naidenliam-code <naidenliam@gmail.com>
2026-01-31 15:05:52 +0100

    Add ExploreTab component with bird categories

---
+++ src/ExploreTab.jsx
@@ -0,0 +1,13 @@
+export default function ExploreTab() {
+  return (
+    <div>
+      <h2>Explorer</h2>
+      <ul>
+        <li>🦅 Rapaces</li>
+        <li>🦆 Oiseaux d’eau</li>
+        <li>🐦 Passereaux</li>
+        <li>🌍 Par région</li>
+      </ul>
+    </div>
+  );
+}
